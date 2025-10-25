4f204a38ef0e37a4b24c37b437e7356a491fc317
Fong <phamthanhphong160607@gmail.com>
2025-10-25 11:11:34 +0700

    migrate account.service

--- backend/config/prisma.js
+++ backend/config/prisma.js
@@ -4,6 +4,10 @@ const prisma = new PrismaClient({
     log: ['warn', 'error']
 })
 
+BigInt.prototype.toJSON = function () {
+    return this.toString();
+};
+
 process.on('beforeExit', async () => {
     await prisma.$disconnect()
 })
